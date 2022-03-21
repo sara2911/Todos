bc213f19a4df18f1bce069434fbddabe7c57a6ae
Sara Araby <saraarabyahmed2911@gmail.com>
2022-03-21 18:10:44 +0200

    Add backend

---
+++ TodoWebApi/Todo/Controllers/FilterTask.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class FilterTask : ApiController
+    {
+        private ToDoListEntities db = new ToDoListEntities();
+
+        // GET: api/FilterTask
+        public IQueryable<Task> GetTasks()
+        {
+            return db.Tasks;
+        }
+
+        // GET: api/FilterTask/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult GetTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // PUT: api/FilterTask/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTask(int id, Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FilterTask
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult PostTask(Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tasks.Add(task);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = task.Id }, task);
+        }
+
+        // DELETE: api/FilterTask/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult DeleteTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+
+            return Ok(task);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TaskExists(int id)
+        {
+            return db.Tasks.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TodoWebApi/Todo/Controllers/TasksController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class TasksController : ApiController
+    {
+        private ToDoListEntities db = new ToDoListEntities();
+
+        // GET: api/Tasks
+        public IQueryable<Task> GetTasks()
+        {
+            return db.Tasks;
+        }
+
+        // GET: api/Tasks/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult GetTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // PUT: api/Tasks/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTask(int id, Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Tasks
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult PostTask(Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tasks.Add(task);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = task.Id }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult DeleteTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+
+            return Ok(task);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TaskExists(int id)
+        {
+            return db.Tasks.Count(e => e.Id == id) > 0;
+        }
+
+        //////////
+        
+
+    }
+}
